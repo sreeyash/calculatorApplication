104c104cae2e0121d46743e018f3b707d441c5bc
sreeyash <57933345+sreeyash@users.noreply.github.com>
2021-10-14 12:57:46 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,17 @@
-public class hello
+public class calci
 {
 public static void main(String args[])
 {
-System.out.println("hello sreeya");
+  int a=100;
+  int b=50;
+  
+  System.out.println("Product:"+(a*b));
+  
+  System.out.println("Quotient:"+(a/b));
+  
+  System.out.println("Remainder:"+(a%b));
+  
+  
+  
 }
 }
