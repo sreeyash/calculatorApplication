180368867877fa9cac76cf2c6fa117214e17fce5
sreeya <sreeya123@gmail.com>
2021-10-14 14:28:08 +0530

    Final commit

---
+++ calci.java
@@ -0,0 +1,17 @@
+public class calci
+{
+public static void main(String args[])
+{
+  int a=100;
+  int b=50;
+  
+  System.out.println("Product:"+(a*b));
+  
+  System.out.println("Quotient:"+(a/b));
+  
+  System.out.println("Remainder:"+(a%b));
+  
+  
+  
+}
+}
